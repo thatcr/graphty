3d2a579cc13bd695a6cd6c5b1b20a8c5affbe77d
Rob Thatcher <thatcr@gmail.com>
2021-03-10 11:25:20 +0000

    factor out _from_call don't use @classmethod decorator outside a class wrap in the dict to make typeguard work.

--- src/snake/shifter/key_type.py
+++ src/snake/shifter/key_type.py
@@ -9,6 +9,13 @@ from typing import Type
 from .typing import CallKey
 
 
+def _from_call(cls: Any, *args: Any, **kwargs: Any) -> CallKey:
+    """Build a call key by unpacking functionc all arguments."""
+    bound = cls.__signature__.bind(*args, **kwargs)
+    bound.apply_defaults()
+    return cast(CallKey, cls(*bound.arguments.values()))
+
+
 def make_key_type(func: Callable[..., Any]) -> Type[CallKey]:
     """Construct a type representing a functions signature."""
     sig = inspect.signature(func)
@@ -29,15 +36,6 @@ def make_key_type(func: Callable[..., Any]) -> Type[CallKey]:
     def _repr(self: Any) -> str:
         return repr_fmt.format(*self[:-1])
 
-    # build the tuple from a call signature - note we can't use
-    # the new/init from the named tuple as it won't unpack the
-    # same way as the function call.
-    @classmethod  # type: ignore[misc]
-    def from_call(cls: Any, *args: Any, **kwargs: Any) -> CallKey:
-        bound = sig.bind(*args, **kwargs)
-        bound.apply_defaults()
-        return cast(CallKey, key_type(*bound.arguments.values()))
-
     key_type = type(
         func.__name__,
         (
@@ -54,7 +52,7 @@ def make_key_type(func: Callable[..., Any]) -> Type[CallKey]:
             "__func__": func,
             "__module__": func.__module__,
             "__signature__": sig,
-            "from_call": from_call,
+            "from_call": classmethod(_from_call),
         },
     )
 
